f9a85eac0be97046e04565d04575308d117b21de
sajadzirak <iriafmig29@gmail.com>
2023-10-27 19:12:38 +0330

    building home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,91 @@
+import "./App.css";
+
 function App() {
-  return <div>
-    test
-  </div>
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          MyBlog
+        </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2023/10/Soundboks.jpg?w=990&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>
+            Soundboks is scaling high-volume, high-energy speakers across
+            borders and cultures
+          </h2>
+          <p className="info">
+            <a className="author">Matt Burns</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            norway-based Soundboks just released its latest $1,000 Bluetooth
+            speaker. Now in its fourth generation, the latest Soundboks speaker
+            is just like the older models: It’s a large, unabashed black box
+            designed to withstand a rowdy, off-the-grid weekend of music.{" "}
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2023/10/Soundboks.jpg?w=990&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>
+            Soundboks is scaling high-volume, high-energy speakers across
+            borders and cultures
+          </h2>
+          <p className="info">
+            <a className="author">Matt Burns</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            norway-based Soundboks just released its latest $1,000 Bluetooth
+            speaker. Now in its fourth generation, the latest Soundboks speaker
+            is just like the older models: It’s a large, unabashed black box
+            designed to withstand a rowdy, off-the-grid weekend of music.{" "}
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img
+            src="https://techcrunch.com/wp-content/uploads/2023/10/Soundboks.jpg?w=990&crop=1"
+            alt=""
+          />
+        </div>
+        <div className="texts">
+          <h2>
+            Soundboks is scaling high-volume, high-energy speakers across
+            borders and cultures
+          </h2>
+          <p className="info">
+            <a className="author">Matt Burns</a>
+            <time>2023-01-06 16:45</time>
+          </p>
+          <p className="summary">
+            norway-based Soundboks just released its latest $1,000 Bluetooth
+            speaker. Now in its fourth generation, the latest Soundboks speaker
+            is just like the older models: It’s a large, unabashed black box
+            designed to withstand a rowdy, off-the-grid weekend of music.{" "}
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
